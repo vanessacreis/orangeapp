88a405c6ccc94e313cec2d3a4603dec15403633b
vanessacreis <vanessacreis@live.com>
2022-06-06 20:06:02 -0300

    feat base button component

---
+++ src/components/Button/style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { light } from "../../style/theme";
+
+export const Button = styled.button`
+  background-color: ${light.primary};
+  color: ${light.white};
+  font-weight: 400;
+  letter-spacing: 0.1rem;
+  font-family: ${light.text};
+  cursor: pointer;
+  box-shadow: 0 2px 10px ${light.black};
+
+  &.circle {
+    width: 5.5rem;
+    height: 5.5rem;
+    border: none;
+    border-radius: 2.5rem;
+  }
+
+  &.large {
+    height: 4.5rem;
+    width: 20rem;
+    border: none;
+    border-radius: 2.5rem;
+  }
+`;
