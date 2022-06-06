e28b21f07e0f408ea2c64f77a80b4bd447e76f21
vanessacreis <vanessacreis@live.com>
2022-06-06 20:06:23 -0300

    feat basic header

---
+++ src/components/Header/style.ts
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { light } from "../../style/theme";
+
+export const Header = styled.header`
+  width: 100%;
+  height: 7.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 2.5rem;
+
+  a {
+    cursor: pointer;
+  }
+
+  img {
+    max-width: 15rem;
+  }
+`;
+
+export const Nav = styled.nav`
+  width: 10%;
+
+  .nav_link {
+    color: ${light.text};
+    text-decoration: none;
+    text-transform: uppercase;
+
+    &.active {
+      color: ${light.primary};
+    }
+
+    &:hover {
+      color: ${light.primary};
+    }
+  }
+`;
