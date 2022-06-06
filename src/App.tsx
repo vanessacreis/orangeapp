28bc7184008de74f929dc59fe08bbd3e520eb2fc
vanessacreis <vanessacreis@live.com>
2022-06-06 20:09:34 -0300

    fix remove theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { GlobalStyle } from "./style/globalStyle";
-import { light } from "./style/theme";
 
 function App() {
-  const [theme, setTheme] = useState(true);
-
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme ?? light} />
       <GlobalStyle />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
