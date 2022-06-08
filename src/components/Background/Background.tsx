fab203706383fc82b0367c168bbee9de76929c05
vanessacreis <vanessacreis@live.com>
2022-06-08 19:20:22 -0300

    feat background component

---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,23 @@
+import * as S from "./style";
+
+const Background = () => {
+  return (
+    <S.Background>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </S.Background>
+  );
+};
+
+export default Background;
