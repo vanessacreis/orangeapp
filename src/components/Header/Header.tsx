925fabcb8309ec57b77be8d890c71b8e26e65767
vanessacreis <vanessacreis@live.com>
2022-06-09 14:31:43 -0300

    fix header nav

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,53 @@
 import * as S from "./style";
 import logoImg from "../../assets/images/orange-2.png";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { isAuth } from "../../routes/auth";
+import { useAppointments } from "../../hooks/useAppointments";
 
 const Header = () => {
+  const auth = isAuth();
+  const context = useAppointments();
+
   return (
-    <S.Header>
-      <Link to="/">
-        <img src={logoImg} alt="Logo Orange App" />
-      </Link>
-      <S.Nav>
-          <NavLink to='/login' className={({ isActive }) => `nav_link${isActive ? " active" : ""}`}>
-              Login
+    <S.Header
+      style={
+        !auth ? { backgroundColor: "#fcfcfc" } : { backgroundColor: "#F2A922" }
+      }
+    >
+      <img src={logoImg} alt="Logo Orange App" />
+
+      {!auth ? (
+        <S.Nav>
+          <NavLink
+            to="/"
+            className={({ isActive }) => `nav_link${isActive ? " active" : ""}`}
+          >
+            Home
           </NavLink>
-      </S.Nav>
+          <NavLink
+            to="/signup"
+            className={({ isActive }) => `nav_link${isActive ? " active" : ""}`}
+          >
+            Cadastre-se
+          </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) => `nav_link${isActive ? " active" : ""}`}
+          >
+            Login
+          </NavLink>
+        </S.Nav>
+      ) : (
+        <S.User>
+          <div className="user">
+            <h3>{context.states?.user.name}</h3>
+            <img src={context.states?.user.avatar} alt="Foto do usuário" />
+          </div>
+          <button className="logout" onClick={context.actions?.handleLogout}>
+            Sair
+          </button>
+        </S.User>
+      )}
     </S.Header>
   );
 };
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -8,8 +8,6 @@ export const Header = styled.header`
   align-items: center;
   justify-content: space-between;
   padding-inline: 2.5rem;
-  /* position: absolute;
-  z-index: 2; */
 
   a {
     cursor: pointer;
@@ -21,7 +19,11 @@ export const Header = styled.header`
 `;
 
 export const Nav = styled.nav`
-  width: 10%;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 30%;
+  padding-inline: 3rem;
 
   .nav_link {
     color: ${light.text};
@@ -29,11 +31,54 @@ export const Nav = styled.nav`
     text-transform: uppercase;
 
     &.active {
+      font-weight: bold;
       color: ${light.primary};
     }
 
     &:hover {
       color: ${light.primary};
+    }
+  }
+
+  .nav_link::after {
+    content: "";
+    width: 0;
+    height: 2px;
+    background-color: ${light.primary};
+    display: block;
+    position: relative;
+    bottom: -1rem;
+    left: -0.5rem;
+    transition: width 0.2s;
+  }
+
+  .nav_link:hover::after,
+  .nav_link.active::after {
+    width: 100%;
+    padding-inline: 0.8rem;
+  }
+`;
+
+export const User = styled.div`
+  width: 20%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .user {
+    display: flex;
+    align-items: center;
+
+    > h3 {
+      font-family: ${light.fontTitle};
+      font-size: 1.8rem;
+      font-weight: 300;
+      margin-right: 1rem;
+    }
+    > img {
+      width: 3.5rem;
+      height: 3.5rem;
+      border-radius: 100rem;
     }
   }
 `;
