2f7525d7ed587d5affbb89d3a0a5d412ca3a195f
vanessacreis <vanessacreis@live.com>
2022-06-10 01:06:54 -0300

    refactor importe routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Routes from "./routes/routes";
+import Routes from "./routes/Routes";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
