cd8842658f7b9cd3de957e5098d1f27549b61df5
vanessacreis <vanessacreis@live.com>
2022-06-10 00:02:12 -0300

    fix add crud

--- src/pages/YourHome/YourHome.tsx
+++ src/pages/YourHome/YourHome.tsx
@@ -1,62 +1,133 @@
-import { useEffect, useState } from "react";
+import { useEffect, useId, useState } from "react";
 import Card from "../../components/Card/Card";
 import { useAppointments } from "../../hooks/useAppointments";
 import { api } from "../../services/api";
-import { TAppointment } from "../../types/types";
+import { TAppointment, TAppointmentArray } from "../../types/types";
 import * as S from "./style";
 import emptyImg from "../../assets/images/empty-box.png";
+import plusImg from "../../assets/images/plus-icons.svg";
+import Button from "../../components/Button/Button";
+import Modal from "../../components/Modal/Modal";
+import dayjs from "dayjs";
+
+const initialAppointment: TAppointment = {
+  id: 0,
+  uuid: "",
+  title: "",
+  description: "",
+  createdAt: dayjs(new Date()).format("MMM DD YYYY HH:mm"),
+  start: {
+    day: "",
+    time: "",
+  },
+  end: {
+    day: "",
+    time: "",
+  },
+};
 
 const YourHome = () => {
-  const [appointments, setAppointments] = useState([]);
   const context = useAppointments();
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const appointments = context.states?.appointments;
+  const [appointmentsRender, setAppointmentsRender] =
+    useState<any>(appointments);
+  const [todayAppointment, settodayAppointment] = useState<any>(appointments);
+  const [load, setLoad] = useState(false);
   const user = context.states?.user;
 
-  const request = async () => {
-    const response = await api.get("/appointments");
-    const { data } = response;
-    const filtered = data.filter(
-      (appointment: TAppointment) => appointment.uuid === user?.uuid
-    );
-    setAppointments(filtered);
-  };
+  function handlePostAppointment(appointment: TAppointment) {
+    context.actions?.handleCreateAppointment(appointment);
+    setAppointmentsRender([...appointmentsRender, appointment]);
+    setIsModalVisible(false);
+  }
+
+  function handleDeleteAppointment(id: number) {
+    context.actions?.handleDeleteAppointment(id);
+    const filtered = appointments?.filter((app) => app.id != id);
+    setAppointmentsRender(filtered);
+    setIsModalVisible(false);
+  }
+
+  function handleTodayAppointments(appointments?: TAppointmentArray) {
+    const today = dayjs(new Date()).format("YYYY-MM-DD");
+    if (!!appointments) {
+      const todayAppointments = appointments.filter(
+        (appointment: TAppointment) => appointment.start.day === today
+      );
+      settodayAppointment(todayAppointments);
+    }
+  }
 
   useEffect(() => {
-    request();
-  }, []);
+    handleTodayAppointments(appointments);
+    setAppointmentsRender(appointments);
+  }, [appointments]);
 
   return (
     <S.Container>
       <S.Today>
         <h1>Bem vindo {user?.name}!</h1>
-        <p>Você tem 2 compromissos agendados para hoje.</p>
+        <p>
+          Você tem {todayAppointment.length} compromisso(s) agendado(s) para
+          hoje.
+        </p>
         <S.CardList>
-        {!appointments ? (
+          {load ? (
             <div className="empty">
-              <p>Opa, não tem nada aqui!</p>
+              <p>Você não tem compromissos hoje!</p>
               <img src={emptyImg} alt="Imagem de caixa vazia" />
             </div>
           ) : (
-            appointments.map((appointment: TAppointment) => {
-              return <Card key={appointment.id} appointment={appointment} />;
+            todayAppointment.map((appointment: TAppointment) => {
+              return (
+                <Card
+                  key={appointment.id}
+                  appointment={appointment}
+                  buttonDelete={handleDeleteAppointment}
+                />
+              );
             })
           )}
-
         </S.CardList>
       </S.Today>
       <S.Schedules>
-        <h2>Todos os compromissos</h2>
+        <div className="box">
+          <h2>Todos os compromissos</h2>
+          <Button
+            className="circle plus"
+            onClick={() => setIsModalVisible(true)}
+          >
+            <img src={plusImg} alt="Incluir novo agendamento" />
+          </Button>
+        </div>
         <S.CardList>
-          {!appointments ? (
+          {!appointmentsRender ? (
             <div className="empty">
               <p>Opa, não tem nada aqui!</p>
               <img src={emptyImg} alt="Imagem de caixa vazia" />
             </div>
           ) : (
-            appointments.map((appointment: TAppointment) => {
-              return <Card key={appointment.id} appointment={appointment} />;
+            appointmentsRender.map((appointment: TAppointment) => {
+              return (
+                <Card
+                  key={appointment.id}
+                  appointment={appointment}
+                  buttonDelete={handleDeleteAppointment}
+                />
+              );
             })
           )}
         </S.CardList>
+        {isModalVisible && (
+          <Modal
+            buttonDelete={() => console.log()}
+            type="new"
+            onClose={() => setIsModalVisible(false)}
+            buttonSave={handlePostAppointment}
+            appointment={initialAppointment}
+          />
+        )}
       </S.Schedules>
     </S.Container>
   );
--- src/pages/YourHome/style.ts
+++ src/pages/YourHome/style.ts
@@ -3,7 +3,7 @@ import { light } from "../../style/theme";
 
 export const Container = styled.main`
   background-color: ${light.background};
-  min-height: calc(100vh - 7.5rem);
+  height: calc(100vh - 7.5rem);
   display: flex;
 `;
 
@@ -25,19 +25,43 @@ export const Today = styled.section`
 export const Schedules = styled.section`
   flex: 1;
   padding-top: 2.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-  > h2 {
-    color: ${light.primary};
-    font-size: 2.5rem;
-    font-family: ${light.fontTitle};
-    font-weight: 300;
+  .box {
+    width: 50rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > h2 {
+      color: ${light.primary};
+      font-size: 2.5rem;
+      font-family: ${light.fontTitle};
+      font-weight: 300;
+    }
   }
 `;
 
 export const CardList = styled.div`
+  height: 80%;
+  width: 55rem;
   display: flex;
   flex-direction: column;
   gap: 2rem;
   margin-top: 4rem;
   overflow-y: auto;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+    height: 8px;
+  }
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px transparent;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: ${light.secundary};
+    border-radius: 5px;
+  }
 `;
